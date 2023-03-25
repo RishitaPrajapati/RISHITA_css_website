62ef0fbd91102ed0fdc92f307d36ec68ebb4d742
RISHITA <118433270+RishitaPrajapati@users.noreply.github.com>
2023-03-25 15:54:44 +0530

    Delete rishita css website directory

--- rishita css website/cntact-us.js
+++
@@ -1,7 +0,0 @@
-function form_valid(){
-    let fname=document.form1.fname.value;
-    if( fname==""){
-        document.getElementById("fname_mess").innerHTML="enter your name)"
-        return false;
-    }
-}
